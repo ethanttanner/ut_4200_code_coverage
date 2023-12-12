f8a6110ce8861b63b851e4676a74fc0d4792db25
Ethan Tanner <ethanntanner@gmail.com>
2023-12-12 14:36:38 -0700

    config

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-CIRCLECI_API_TOKEN="CCIPAT_6TZjtzwDmzZQn9RQS2RNVf_e58969f1485e331d6a2abf832b761458d9aa3270"
-PROJECT_USERNAME="cci-69y7kk"
-PROJECT_NAME="ut_4200_code_coverage"
-BUILD_NUMBER="036aa36"
+API_TOKEN="CCIPAT_6TZjtzwDmzZQn9RQS2RNVf_e58969f1485e331d6a2abf832b761458d9aa3270"
+USERNAME="cci-69y7kk"
+PROJECT="ut_4200_code_coverage"
+BUILD_NUMBER="7c6c841"
 
-response=$(curl -X POST "https://circleci.com/api/v2/project/github/$PROJECT_USERNAME/$PROJECT_NAME/pipeline" \
--H "Content-Type: application/json" \
--H "Circle-Token: $CIRCLECI_TOKEN")
+# Authenticate
+curl -X POST "https://circleci.com/api/v2/me/prefs" \
+     -H "Circle-Token: $CIRCLECI_TOKEN"
 
-# Extract pipeline ID
-pipeline_id=$(echo $response | jq -r .id)
+# Retrieve test results
+curl -u "${API_TOKEN}:" "https://circleci.com/api/v2/project/github/${USERNAME}/${PROJECT}/${BUILD_NUMBER}/test-results" -o tests.zip
 
-# Retrieve artifacts
-curl -X GET "https://circleci.com/api/v2/project/github/$PROJECT_USERNAME/$PROJECT_NAME/pipeline/$pipeline_id/job/BUILD_NUM/test_results" \
--H "Content-Type: application/json" \
--H "Circle-Token: $CIRCLECI_TOKEN" \
--o artifacts.zip
-
+# Retrieve coverage report
+curl -u "${API_TOKEN}:" "https://circleci.com/api/v2/project/github/${USERNAME}/${PROJECT}/${BUILD_NUMBER}/coverage" | jq -r '.items[] | select(.path | contains("coverage")) | .url' | xargs curl -u "${API_TOKEN}:" -o coverage.zip
