2f2f37d5e378a0613fa4fdb82fa2771bc46e3111
Ethan Tanner <ethanntanner@gmail.com>
2023-12-12 15:26:03 -0700

    script

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 API_TOKEN="CCIPAT_6TZjtzwDmzZQn9RQS2RNVf_e58969f1485e331d6a2abf832b761458d9aa3270"
-USERNAME="cci-69y7kk"
+USERNAME="ethanttanner"
 PROJECT="ut_4200_code_coverage"
-BUILD_NUMBER="7c6c841"
+BUILD_NUMBER="5ebac54"
 
 # Authenticate
 curl -X POST "https://circleci.com/api/v2/me/prefs" \
-     -H "Circle-Token: $CIRCLECI_TOKEN"
+     -H "Circle-Token: $API_TOKEN"
+
 
 # Retrieve test results
 curl -u "${API_TOKEN}:" "https://circleci.com/api/v2/project/github/${USERNAME}/${PROJECT}/${BUILD_NUMBER}/test-results" -o tests.zip
